11b1573f0427f2d50dc86389cf8ca47406333f9b
Mike McTighe <m.mctighe10@gmail.com>
2019-10-12 00:39:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT specie, COUNT(*) FROM characters GROUP   "
+  "SELECT specie, COUNT(*) FROM characters GROUP BY   "
 end
 
 def select_name_and_series_subgenres_of_authors
